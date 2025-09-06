b2fc52dfd3a22b712d2728d6d0bdf7a54d900b16
João Pedro Lima <jpbenevideslima0707@gmail.com>
2025-09-06 13:34:17 -0300

    first commit

---
+++ Programa.java
@@ -0,0 +1,10 @@
+
+import java.util.List;
+
+public class Programa {
+    
+    private List<String> variaveis;
+    private List<String> funcoes;
+    private int acumulador;
+    private int posicao;
+}
